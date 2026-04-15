5ec34a20576e338b1174ce0870b939cb3c21dc9e
Dollant <ariawilliams2425@gmail.com>
2026-04-15 12:40:20 -0400

    fixed myPhone to yourPhone on the second Cell Phone

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -11,11 +11,11 @@ public class CellPhoneApplication {
         myPhone.setOwner("Taryn Williams");
 
         CellPhone yourPhone = new CellPhone();
-        myPhone.setSerialNumber(5000005);
-        myPhone.setModel("Samsung Galaxy Ultra 25");
-        myPhone.setCarrier("Mint Mobile");
-        myPhone.setPhoneNumber("(666) 777-6767");
-        myPhone.setOwner("Fia Reaves");
+        yourPhone.setSerialNumber(5000005);
+        yourPhone.setModel("Samsung Galaxy Ultra 25");
+        yourPhone.setCarrier("Mint Mobile");
+        yourPhone.setPhoneNumber("(666) 777-6767");
+        yourPhone.setOwner("Fia Reaves");
 
         System.out.println("Serial #: " + myPhone.getSerialNumber());
         System.out.println("Model: "    + myPhone.getModel());
@@ -25,8 +25,18 @@ public class CellPhoneApplication {
 
         display(myPhone);
         display(yourPhone);
+
         myPhone.dial(yourPhone.getPhoneNumber());
         yourPhone.dial(myPhone.getPhoneNumber());
     }
 
+    public static void display(CellPhone phone) {
+        System.out.println("°❀⋆.ೃ࿔*:･°❀⋆.ೃ࿔*:･────୨ৎ────°❀⋆.ೃ࿔*:･°❀⋆.ೃ࿔*:･");
+        System.out.println("Owner: " + phone.getOwner());
+        System.out.println("Model: " + phone.getModel());
+        System.out.println("Carrier: " + phone.getCarrier());
+        System.out.println("Phone #: " + phone.getPhoneNumber());
+        System.out.println("Serial #: " + phone.getSerialNumber());
+        System.out.println("°❀⋆.ೃ࿔*:･°❀⋆.ೃ࿔*:･────୨ৎ────°❀⋆.ೃ࿔*:･°❀⋆.ೃ࿔*:･\n");
+    }
 }
