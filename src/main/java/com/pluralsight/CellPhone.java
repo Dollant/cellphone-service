574a5211579ee011a867a01fafa80efdfd975648
Dollant <ariawilliams2425@gmail.com>
2026-04-15 13:16:00 -0400

    Cleanup of code to take up less lines.

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -16,38 +16,20 @@ public class CellPhone {
         owner = "";
     }
 
-    public int getSerialNumber() {
-        return serialNumber;
-    }
-    public String getModel() {
-        return model;
-    }
-    public String getCarrier() {
-        return carrier;
-    }
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-    public String getOwner() {
-        return owner;
-    }
+    public int getSerialNumber() { return serialNumber; }
+    public String getModel() { return model; }
+    public String getCarrier() { return carrier; }
+    public String getPhoneNumber() { return phoneNumber; }
+    public String getOwner() { return owner; }
 
-    public void setSerialNumber(int serialNumber) {
-        this.serialNumber = serialNumber;
-    }
-    public void setModel (String model) {
-        this.model = model;
-    }
-    public void setCarrier (String carrier) {
-        this.carrier = carrier;
-    }
-    public void setPhoneNumber (String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-    public void setOwner (String owner) {
-        this.owner = owner;
-    }
+    public void setSerialNumber(int serialNumber) { this.serialNumber = serialNumber; }
+    public void setModel (String model) { this.model = model; }
+    public void setCarrier (String carrier) { this.carrier = carrier; }
+    public void setPhoneNumber (String phoneNumber) { this.phoneNumber = phoneNumber; }
+    public void setOwner (String owner) { this.owner = owner; }
+
     public void dial(String phoneNumber) {
         System.out.println(owner + "'s phone is calling (˶˃ ᵕ ˂˶) ♡ " + phoneNumber);
     }
+
 }
