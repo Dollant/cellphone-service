75269e29c1bc75b73d0351db2a8b3b87571ffdb0
Dollant <ariawilliams2425@gmail.com>
2026-04-15 11:05:28 -0400

    Instantiated CellPhone object and assign values using setter methods so, new CellPhone() = blueprint becomes a real object in memory.

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -3,5 +3,13 @@ package com.pluralsight;
 public class CellPhoneApplication {
     public static void main(String[] args) {
 
+        CellPhone myPhone = new CellPhone();
+
+        myPhone.setSerialNumber(4000004);
+        myPhone.setModel("iPhone 17 Pro Max");
+        myPhone.setCarrier("Verizon");
+        myPhone.setPhoneNumber("(420) 069-6767");
+        myPhone.setOwner("Taryn Williams");
+
     }
 }
